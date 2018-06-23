db9d5d5133a7ae348cd4ca543ce0edecea045086
Mark Cantrill <mark@astro-designs.com>
2018-06-23 21:10:19 +0100

    Updating setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-if not exist /boot/hostname.txt
+if [ ! -f /boot/hostname.txt ]
+then
+    echo Adding hostname.txt
     sudo cp ./hostname.txt /boot
+fi
 
-if exist /boot/autorun.sh
-    sudo cp /boot/autorun.sh /boot/autorun.old -y
-    
-if not exist /boot/autorun.sh
+if [ ! -f /boot/autorun.sh ]
+then
+    echo Adding new autorun.sh
     sudo cp ./autorun.sh /boot
+fi
+    
+if [ -f /boot/autorun.sh ]
+then
+    echo Replacing autorun.sh
+    sudo cp /boot/autorun.sh /boot/autorun.old -f
+    sudo cp ./autorun.sh /boot
+fi
     
